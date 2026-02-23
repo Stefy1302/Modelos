10193ee7a790089ce439d467f1e342d3c2e14318
Stefany Morales <sdmoralesm@udistrital.edu.co>
2026-02-23 16:11:59 -0500

    Harry Potter

---
+++ Documents/Stefy/Modelos-1.2/Modelos-1.2/StrategyHarryPotter/src/main/java/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import Spells.*;
+import Wizards.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Wizard harry = new HarryPotter("Harry Potter");
+        Wizard hermione = new HermioneGranger("Hermione Granger");
+        Wizard ron = new RonWeasley("Ron Weasley");
+
+        harry.display();
+        harry.performSpells();
+
+        hermione.display();
+        hermione.performSpells();
+
+        ron.display();
+        ron.performSpells();
+
+        hermione.setSpellBehavior(new SpellOculusReparo());
+
+        hermione.display();
+        hermione.performSpells();
+
+    }
+}
---
+++ Documents/Stefy/Modelos-1.2/Modelos-1.2/StrategyHarryPotter/src/main/java/Spells/SpellExpelliarmus.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//SpellsExpelliarmus
+package Spells;
+
+public class SpellExpelliarmus implements SpellBehavior {
+
+    @Override
+    public void castSpell() {
+        System.out.println("You cast Expelliarmus! A flash of light disarms the opponent.");
+    }
+}
